8909dee501a60734969df82d0022cf95aa5a0cc6
Miguel Moreo <miguelangel.moreo@gmail.com>
2020-11-15 16:02:47 +0100

    Added integer tests

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,6 +1,9 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
 	sum := Add(2, 2)
@@ -9,4 +12,12 @@ func TestAdder(t *testing.T) {
 	if sum != expected {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
 	}
+}
+
+// Please note that the example function will not be executed if you remove
+// the comment //Output: 6. Although the function will be compiled, it won't be executed.
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
 }
