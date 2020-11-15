99fd765a227dd91f2f9d63b8fb851825a012afa1
Miguel Moreo <miguelangel.moreo@gmail.com>
2020-11-15 11:25:06 +0100

    Refactor: Create constant

--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) { /* t is our hook to the testing environment*/
 	got := Hello("Mike")
-	want := "Hello Mike!"
+	want := "Hello, Mike!"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
