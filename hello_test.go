e2fd71970a070f3c0ad55f72995f9934b668b937
Miguel Moreo <miguelangel.moreo@gmail.com>
2020-11-15 11:20:21 +0100

    first commit

---
+++ hello_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) { /* t is our hook to the testing environment*/
+	got := Hello("Mike")
+	want := "Hello Mike!"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
