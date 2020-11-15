ebd1f5eee09c36c2b69f1fa1df16ea9142ec8425
Miguel Moreo <miguelangel.moreo@gmail.com>
2020-11-15 11:25:06 +0100

    Refactor: Create constant. TEST

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) { /* t is our hook to the testing environment*/
-	got := Hello("Mike")
-	want := "Hello Mike!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper() // tells the test suite that this method is a helper. (line numbers)
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Mike")
+		want := "Hello, Mike!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello to the world", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, SWorld!"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
